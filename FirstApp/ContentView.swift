a209e2894b31b3a5d009b49515899c63ccd399e3
986532qw <986532qw@gmail.com>
2021-03-17 00:22:58 +0300

    Initial commit Tips calculator (First Swift App)

--- FirstApp/ContentView.swift
+++ FirstApp/ContentView.swift
@@ -8,9 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var total = "2000"
+    @State var tipPercent: Double = 15.0
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            Text("Калькулятор чаевых")
+                .font(.title)
+            
+            HStack{
+                Text("₽")
+                TextField("Итог", text: $total)
+                    .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 0.25)
+            }
             .padding()
+            
+            HStack {
+                Slider(value: $tipPercent, in: 1...30, step: 1.0)
+                Text("\(Int(tipPercent))%")
+            }
+            .padding()
+            
+            if let totalNum = Double(total) {
+                Text("Чаевые: ₽\(totalNum * tipPercent / 100, specifier: "%0.2f")")
+            } else {
+                Text("Вы ввели не верное число")
+            }
+        }
     }
 }
 
